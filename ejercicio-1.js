dc3baffc193d2df326c0d94b83adad7975ddeb7f
Rakelika <raquelsg.gp@gmail.com>
2022-12-03 13:47:51 +0100

    Iteration#1

---
+++ ejercicio-1.js
@@ -0,0 +1,7 @@
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+for (var product of products) {
+    if (product.includes("Camiseta")) {
+        console.log(product)
+    }
+  }
